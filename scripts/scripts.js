2fea5efc3da0a43fa56caf30e132f4f6006de61a
t1me1srunn1nout <t1me1srunn1nout@Hovseps-MacBook-Pro.local>
2017-05-05 22:41:39 -0700

    buttons work

---
+++ scripts/scripts.js
@@ -0,0 +1,101 @@
+  // Creates an array that lists out all of the options (Rock, Paper, or Scissors).
+  var options = ["r", "p", "s"];
+  var computerScore = 0;
+  var userScore = 0;
+  var tieScore = 0;
+  // This function is run whenever the user presses a key.
+  document.onkeyup = function(event) {
+    // Determines which key was pressed
+    var userGuess = event.key;
+    // Alerts the key the user pressed (userGuess).
+    console.log(userGuess);
+    // Randomly chooses a choice from the options array. This is the Computer's guess.
+    var computerGuess = options[Math.floor(Math.random() * options.length)];
+    // Alerts the Computer's guess.
+    console.log(computerGuess);
+
+    // if (userGuess === options.indexOf(-1))
+
+    //bands.indexOf() === -1
+    if ((userGuess === "r") || (userGuess === "p") || (userGuess === "s")) {
+      // replace the tie comparisons with indexOf()
+      if (  (userGuess === "r" && computerGuess === "r") ||
+            (userGuess === "s" && computerGuess === "s") ||
+            (userGuess === "p" && computerGuess === "p")) {
+        tieScore++;
+        console.log("TIE TIE");
+      } else if (userGuess === "r" && computerGuess === "s") {
+        userScore++;
+      } else if (userGuess === "r" && computerGuess === "p") {
+        computerScore++;
+      } else if (userGuess === "s" && computerGuess === "r") {
+        computerScore++;
+      } else if (userGuess === "s" && computerGuess === "p") {
+        userScore++;
+      } else if (userGuess === "p" && computerGuess === "r") {
+        userScore++;
+      } else if (userGuess === "p" && computerGuess === "s") {
+        computerScore++;
+      }
+      console.log("User Score = " + userScore);
+      console.log("Computer Score = " + computerScore);
+      console.log("Tie Score = " + tieScore);
+
+      var html = "<h4>You: " + userGuess +
+      " Computer: " + computerGuess + "</h4>" +
+      "<h4>wins: " + userScore + "</h4>" +
+      "<h4>losses: " + computerScore + "</h4>" +
+      "<h4>ties: " + tieScore + "</h4>";
+      document.querySelector("#game").innerHTML = html;
+    }
+  };
+
+
+  var buttonPress = function(buttonValue) {
+    var userGuess = buttonValue;
+    console.log(userGuess);
+
+    var computerGuess = options[Math.floor(Math.random() * options.length)];
+    console.log(computerGuess);
+
+    if ((userGuess === "r") || (userGuess === "p") || (userGuess === "s")) {
+      if (  (userGuess === "r" && computerGuess === "r") ||
+            (userGuess === "s" && computerGuess === "s") ||
+            (userGuess === "p" && computerGuess === "p")) {
+        tieScore++;
+
+      } else if (userGuess === "r" && computerGuess === "s") {
+        userScore++;
+      } else if (userGuess === "r" && computerGuess === "p") {
+        computerScore++;
+      } else if (userGuess === "s" && computerGuess === "r") {
+        computerScore++;
+      } else if (userGuess === "s" && computerGuess === "p") {
+        userScore++;
+      } else if (userGuess === "p" && computerGuess === "r") {
+        userScore++;
+      } else if (userGuess === "p" && computerGuess === "s") {
+        computerScore++;
+      }
+      console.log("User Score = " + userScore);
+      console.log("Computer Score = " + computerScore);
+      console.log("Tie Score = " + tieScore);
+
+      var html = "<h4>You: " + userGuess +
+      " Computer: " + computerGuess + "</h4>" +
+      "<h4>wins: " + userScore + "</h4>" +
+      "<h4>losses: " + computerScore + "</h4>" +
+      "<h4>ties: " + tieScore + "</h4>";
+      document.querySelector("#game").innerHTML = html;
+    }
+  };
+
+  $("#rockClick").click(function(){
+    buttonPress("r");
+  });
+  $("#paperClick").click(function(){
+    buttonPress("p");
+  });
+  $("#scissorClick").click(function(){
+    buttonPress("s");
+  });
